06c055c1cb8ee23d0b238ba897d4fb8ccdfab765
Hudhayfa <hudhayfaabdulrazaq55@gmail.com>
2026-09-02 22:09:16 +0100

    Day-1: first python project

---
+++ Tech-roadmap/Day-01/main.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+goal = input("What's your main goal for this roadmap? ")
+
+print(f"\nWhat's good, {name}!")
+print(f"Your goal: {goal}")
+print("Day 1 officially started. 🚀")
