8596d3aaf9e6d8e42fe35c03a83da18fc76f4031
Hudhayfa <hudhayfaabdulrazaq55@gmail.com>
2026-09-11 19:47:57 +0100

    Day 02 Inputs and variables

---
+++ Tech-roadmap/Day-02/main.py
@@ -0,0 +1,21 @@
+name = "Hudhayfa"
+age = 18
+career = "Computer Engineering"
+
+print(name)
+print(age)
+print(career)
+
+Name = input("What is your father")
+print("Hello",Name)
+
+nAme = input("What is your name? ")
+Age = input("How old are you? ")
+Goal = input("What is your goal? ")
+
+print()
+print("==========JARVIS PROFILE=============")
+print("Name: ",nAme)
+print("Age: ",Age)
+print("Goal: ",Goal)
+print("=============")
