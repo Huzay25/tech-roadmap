8137f8e8202ab2d6f7952eebd29f6b87e8a0da4d
Hudhayfa <hudhayfaabdulrazaq55@gmail.com>
2026-09-18 17:04:37 +0100

    Day-04 Funcs

--- Tech-roadmap/Day-04/Cal.py
+++ Tech-roadmap/Day-04/Cal.py
@@ -1,11 +1,3 @@
-def greet(name):
-    print(f"Welcome to Jarvis {name}")
-greet("Hudhayfa")
-greet('Alexis Sanchez')
-def add(a,b):
-    return a+b
-result = add(10,5)
-print(result)
 def add(a,b):
     return a+b
 def subtract(a,b):
